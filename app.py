c3056702c284d37924d70c16d26b9fa25b602856
PatHealy <pwh5@pitt.edu>
2020-04-10 05:59:48 -0400

    The web app is mostly done, with a few notable to-dos (marked in comments) and needs interface with the DB

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sqlalchemy import inspect
 from sqlalchemy.sql import text
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' #this is just for development purposes -- not a real secret key!
 
 POSTGRES_URL = "127.0.0.1:5432"
 POSTGRES_USER = "postgres"
@@ -37,33 +38,59 @@ def browse_home():
 @app.route('/browse/<type>')
 def browse_stuff(type):
 	keys, contents = get_all(type)
+	flash("Viewing " + type.upper())
 	return render_template('browse.html', type=type, keys=keys, contents=contents, order_by=None, order=None)
 
 @app.route('/browse/<type>/<order_by>/<order>')
 def browse_stuff_order_by(type,order_by, order):
 	keys, contents = get_all(type, order_by, order)
+	flash("Viewing " + type.upper() + " ordered by " + order_by.upper() + " in " + order.upper() + " order")
 	return render_template('browse.html', type=type, keys=keys, contents=contents, order_by=order_by, order=order)
 
-@app.route('/create')
-def create_home():
-	return render_template('create.html')
+@app.route('/create/<type>')
+def create(type):
+	types = {} # TODO: get the 
+	return render_template('create.html', table=type, types=types)
+
+@app.route('/create', methods=['POST'])
+def create_new_entry():
+	# TODO: process the sent data 
+	# do the row creation and notify the user if it was successful with the flash() method
+	pass
+
+@app.route('/edit/<type>/<pk>')
+def edit(type, pk):
+	content = {} # TODO: get the current values of this entry
+	types = {} # TODO: get the types of this entry
+	return render_template('edit.html', table=type, primary_key=pk, content=content, types=types)
+
+@app.route('/update', methods=['POST'])
+def update():
+	# TODO: process the sent data 
+	# do the update and notify the user if it was successful with the flash() method
+	pass
 
 def get_all(type, order_by='_na', order='_na'):
-	print("Order by " + order_by + ", " + order)
+	# TODO: actually interact with the database here
 	if order == 'DESC':
 		#call query with DESC
 		pass
-	else:
+	elif order == 'ASC':
 		#call query without DESC
+		pass
+	else:
+		#call query without ordering
 		pass
 	return get_example()
 
 def get_example():
+	#This method is just a placeholder until we actually interface with the DB
 	keys = ['name', 'a', 'b']
 
 	contents = []
 	contents.append(['Example Item 1', 'example a', 'example b'])
 
+	#This commented out bit shows how to interact with the DB
 	#with engine.connect() as con:
 	#	statement = text("""SELECT * FROM example;""")
 	#	rs = con.execute(statement)
